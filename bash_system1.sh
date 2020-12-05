403ee34bbf3980caa1ce0b7d3aab53d65b03ee93
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2020-12-05 03:36:31 +0100

    Update bash_system1.sh

--- bash_system1.sh
+++ bash_system1.sh
@@ -3,8 +3,8 @@ app-script-ch11@challenge02:~$ PATH=/tmp #changer la valeur de la variable d'env
 app-script-ch11@challenge02:~$ echo $PATH #afficher la var d'environnement 
 app-script-ch11@challenge02:~$ ./ch11 #executer le programme le flag devrais s'afficher.
 
-rappel legende:
+:rappel legende:
 env = environnement
 ls = liste le contenu d'un répertoire
 cat = lire un contenu textuel
-PATH = var d'environnement
+PATH = var d'environnement'
